a2f8b3b4765c28f5f6778c3308c1f085d6f99e1c
gitinintoit <ravi.shyam@timesinternet.in>
2021-09-23 15:49:40 +0530

    Adding link to home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
+import styles from '../src/components/styles.module.css'
 
-const mainPage= ()=><div>Home Page</div>
+const mainPage = () => {
+    return (
+        <div className={ styles }>
+            <h1>Home Page</h1>
+            <Link href='/notes'>
+                <a>Notes</a>
+            </Link>
+        </div>
+    )
+}
 
 export default mainPage;
