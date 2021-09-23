dfb6f154b098199560ea0e09e6b398b4dd5e2c00
gitinintoit <ravi.shyam@timesinternet.in>
2021-09-23 15:56:22 +0530

    Updating homepage with inline styling with theme-ui

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,11 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import Link from 'next/link'
-import styles from '../src/components/styles.module.css'
 
-const mainPage = () => {
-    return (
-        <div className={ styles }>
-            <h1>Home Page</h1>
-            <Link href='/notes'>
-                <a>Notes</a>
-            </Link>
-        </div>
-    )
-}
-
-export default mainPage;
+export default () => (
+  <div sx={{ height: `calc(100vh - 60px)`}}>
+    <div sx={{variant: 'containers.page', display: 'flex', alignItems: 'center', height: '100%'}}>
+      <h1 sx={{fontSize: 8, my: 0}}>This is a really dope note taking app.</h1>
+    </div>
+  </div> 
+)
