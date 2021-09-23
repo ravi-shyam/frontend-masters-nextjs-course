fab930852883b5ce02bfa03769eb38736293e985
gitinintoit <ravi.shyam@timesinternet.in>
2021-09-23 15:56:54 +0530

    Adding styles and links to all note pages

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import Link from 'next/link'
 
-const notePage = () => {
-    return (
-        <div>
-            <h1>Notes index page</h1>
-            <Link href='/notes/[id]' as={`/notes/1`}>
-                Note 1
-            </Link>
-        </div>
-    )
-}
+export default () => {
+  const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `This is my note ${i}`}))
 
-export default notePage
+  return (
+    <div sx={{variant: 'containers.page'}}>
+      <h1>My Notes</h1>
+
+      <div sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap'}}>
+        {notes.map(note => (
+          <div sx={{width: '33%', p: 2}}>
+            <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+              <a sx={{textDecoration: 'none', cursor: 'pointer'}}>
+                <div sx={{variant: 'containers.card',}}>
+                  <strong>{note.title}</strong>
+                </div>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
