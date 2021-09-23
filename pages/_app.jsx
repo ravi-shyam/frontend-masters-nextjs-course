eaa9dbec3c59111e27b8f0cd4af62acdb3ab2d62
gitinintoit <ravi.shyam@timesinternet.in>
2021-09-23 16:07:52 +0530

    Adding nav component to the app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import { ThemeProvider } from 'theme-ui'
 import theme from '../theme'
+import Nav from '../src/components/nav'
 
-export default function Application({ Component, props }) {
-    return (
-        <ThemeProvider theme={theme}>
-            <Component {...props}/>
-        </ThemeProvider>
-    )
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <Nav />
+        <Component {...pageProps} />
+      </div>      
+    </ThemeProvider>
+  )
 }
